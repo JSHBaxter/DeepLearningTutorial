3d2c72689c8f861c3be32e68b671431f681074d3
JSHBaxter <jshbaxter@gmail.com>
2019-05-29 13:50:19 +0200

    Finished first draft of E2

--- exercise2.py
+++ exercise2.py
@@ -3,86 +3,137 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import time
 
-#generate image
-def generate_image(size, snr):
-    img = np.zeros((size,size),dtype='float32')
-    idx_x, idx_y = np.meshgrid(range(size),range(size))
-    idx_x, idx_y = idx_x / size, idx_y / size
-    img[ idx_x >= idx_y ] = -1.0
-    img[ idx_x < idx_y ] = 1.0
-    img[ (idx_x-0.75)**2 + (idx_y-0.25)**2 < 0.2**2 ] = 0.0
-    img[ (idx_x-0.25)**2 + (idx_y-0.75)**2 < 0.2**2 ] = 0.0
-    return img+np.random.normal(size=img.shape) / snr
 
-def generate_mask(size):
-    img = np.zeros((size,size),dtype='int32')
-    idx_x, idx_y = np.meshgrid(range(size),range(size))
+# Generate image
+def generate_image(size, snr, radius1, radius2, rotate, flip):
+    img = np.zeros((size,size), dtype='float32')
+    idx_x, idx_y = np.meshgrid(range(size), range(size))
     idx_x, idx_y = idx_x / size, idx_y / size
-    img[ idx_x >= idx_y ] = 0
-    img[ idx_x < idx_y ] = 1
-    img[ (idx_x-0.75)**2 + (idx_y-0.25)**2 < 0.2**2 ] = 2
-    img[ (idx_x-0.25)**2 + (idx_y-0.75)**2 < 0.2**2 ] = 3
+    img[idx_x >= idx_y] = -1.0
+    img[idx_x < idx_y] = 1.0
+    img[(idx_x-0.75)**2 + (idx_y-0.25)**2 < radius1**2] = 0.0
+    img[(idx_x-0.25)**2 + (idx_y-0.75)**2 < radius2**2] = 0.0
+    if rotate:
+        img = np.transpose(img)
+    if flip:
+        img = np.flip(img)
+    return img + np.random.normal(size=img.shape) / snr
+
+
+def generate_mask(size,radius1,radius2, rotate, flip):
+    img = np.zeros((size,size), dtype='int32')
+    idx_x, idx_y = np.meshgrid(range(size), range(size))
+    idx_x, idx_y = idx_x / size, idx_y / size
+    img[idx_x >= idx_y] = 0
+    img[idx_x < idx_y] = 1
+    img[(idx_x-0.75)**2 + (idx_y-0.25)**2 < radius1**2] = 2
+    img[(idx_x-0.25)**2 + (idx_y-0.75)**2 < radius2**2] = 3
+    if rotate:
+        img = np.transpose(img)
+    if flip:
+        img = np.flip(img)
     return img
 
 
-#build database from images
+# Build database from images
 def build_database(number,size,snr):
-    database_i = np.zeros((number,size,size,1),dtype='float32')
-    database_o = np.zeros((number,size,size),dtype='int32')
+    database_i = np.zeros((number,size,size,1), dtype='float32')
+    database_o = np.zeros((number,size,size), dtype='int32')
     for n in range(number):
-        database_i[n,:,:,0] = generate_image(size,snr)
-        database_o[n,:,:] = generate_mask(size)
+        radius1, radius2 = 0.1*np.random.uniform()+0.2, 0.1*np.random.uniform()+0.2
+        rotate, flip = np.random.uniform() > 0.5, np.random.uniform() > 0.5
+        database_i[n,:,:,0] = generate_image(size, snr, radius1, radius2, rotate, flip)
+        database_o[n,:,:] = generate_mask(size, radius1, radius2, rotate, flip)
     return database_i, database_o
 
-#single iteration of a DenseCRF mean field approximation iteration
-def DenseCRF_MeanApproxIteration(Q_in, P_in, k, weight):
-    #perform message passing
-    Q_unweighted = tf.conv2d(Q_in, k)
-    Q_weighted = tf.conv2d(Q_unweighted, weight)
-    Q_weighted = tf.conv2d(Q_weighted, compatibility)
 
-    #add in unary potentials
-    Q_unnormed = P_in - Q_weighted
+# Single iteration of a DenseCRF mean field approximation iteration:
+#
+# Using a fairly dense model with Gaussian weighted edges (with respect to distance). That way, the message passing
+# can be expressed as the same convolution filter (weighted) across all image channels followed by the point-wise
+# compatibility transform. (Two can be combined as a single separable conv2d operation.)
+#
+# We are performing softmax first, that way, q_in/q_out is expressed as logits, same as p_in
+def DenseCRF_MeanApproxIteration(q_in, p_in, kernel, compatibility):
+    # perform softmax on Q to put in [0,1] range
+    q_in = tf.nn.softmax(q_in)
 
-    #normalize solution
-    Q_out = tf.nn.softmax(Q)
-    return Q_out
+    # perform message passing followed by the compatibility transform
+    q_weighted = tf.nn.separable_conv2d(q_in, kernel, compatibility, strides=[1,1,1,1], padding='SAME')
 
+    # add in unary potentials and output
+    q_out = p_in - q_weighted
+    return q_out
+
+
+# Create Gaussian kernel
+def get_gaussian_kernel(size, std):
+    d = tf.distributions.Normal(0.0, std)
+    values = d.prob(tf.range(start=-size, limit=size+1, dtype=tf.float32))
+    gauss_kernel = tf.einsum('i,j->ij', values, values)
+    gauss_kernel = gauss_kernel / tf.reduce_sum(gauss_kernel)
+    gauss_kernel = gauss_kernel[:, :, tf.newaxis, tf.newaxis]
+    return gauss_kernel
+
+
+
+
+# Parameters for training
 learn_rate = 0.01
 iterations = 1000
+crf_iterations = 30
 img_size = 32
 img_snr = 1
 training_size = 25
 testing_size = 25
 
-training_i, training_o = build_database(training_size,img_size,img_snr)
+# Get training set
+training_i, training_o = build_database(training_size, img_size, img_snr)
 
-#build network
-#inputs
-x = tf.placeholder(tf.float32, [None, img_size, img_size,1], name="x")
+# Build network
+# Inputs
+x = tf.placeholder(tf.float32, [None, img_size, img_size, 1], name="x")
 y = tf.placeholder(tf.int32, [None, img_size, img_size], name="y")
 
-#network
-conv1 = tf.layers.conv2d(x,32,3,padding='same',activation=tf.nn.relu)
-conv2 = tf.layers.conv2d(conv1,128,1,padding='same',activation=tf.nn.relu)
-conv3 = tf.layers.conv2d(conv2,256,1,padding='same',activation=tf.nn.relu)
-conv4 = tf.layers.conv2d(conv2,4,1,padding='same')
+# Network
+conv1 = tf.layers.conv2d(x, 32, 3, padding='SAME', activation=tf.nn.relu)
+conv2 = tf.layers.conv2d(conv1, 128, 1, padding='SAME', activation=tf.nn.relu)
+conv3 = tf.layers.conv2d(conv2, 4, 1, padding='SAME')
 
-#weights for CNN
+# Weights for CNN
+k_size = 10
+k = tf.broadcast_to(get_gaussian_kernel(k_size,5.0), [2*k_size+1, 2*k_size+1, 4, 1])
+compatibility = tf.get_variable("compatibility", [1,1,4,4], dtype=tf.float32, initializer=tf.initializers.zeros())
+
+# Iterations for CNN
+Q = conv3
+for i in range(crf_iterations):
+    Q = DenseCRF_MeanApproxIteration(Q, conv3, k, compatibility)
 
 #outputs
-pred_logits = conv4
+pred_logits = Q
 pred_y = tf.nn.softmax(pred_logits)
-acc = 1.0- tf.math.reduce_mean(tf.math.abs(tf.one_hot(y,4)-pred_y))
+out_y = tf.math.argmax(pred_logits,axis=-1)
+acc = 1.0 - tf.math.reduce_mean(tf.math.abs(tf.one_hot(y, 4)-pred_y))
 
-#costs and initializer
-cost =  tf.math.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,logits=pred_logits))
+#confusion matrix
+confusion = tf.math.confusion_matrix(tf.reshape(y, [-1]), tf.reshape(out_y, [-1]), 4)
+
+# Costs and initializers
+cost =  tf.math.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=pred_logits))
 train = tf.train.AdamOptimizer(learn_rate).minimize(cost)
 init = tf.global_variables_initializer()
 
 
-#initializer for network
+# Initializer for network
 init = tf.global_variables_initializer()
+
+
+
+
+
+
+
 
 with tf.Session() as session:
 
@@ -92,15 +143,29 @@ with tf.Session() as session:
     #train network
     for iter in range(iterations+1):
 
-        _, this_cost, this_acc = \
-            session.run([train, cost, acc], feed_dict={
+        _, this_cost, this_acc, this_compatibility, this_confusion, this_y = \
+            session.run([train, cost, acc, compatibility, confusion, out_y], feed_dict={
                 x: training_i,
                 y: training_o
             })
 
+        log_iter = 100
+        if(iter > 0 and iter % log_iter != 1):
+            backstep = 9
+            if crf_iterations == 0:
+                backstep -= 4
+            for j in range(backstep):
+                print("\033[F" + "\033[K", end='')
+        if( iter % log_iter == 0):
+            plt.imshow(this_y[0,:,:])
+            plt.show()
+            time.sleep(0.001)
+
         print(str(iter) + " Losses: " + str(this_cost) + " " + str(this_acc))
+        if crf_iterations > 0:
+            print(str(this_compatibility))
+        print(str(this_confusion))
 
 
 
-while(True):
-    time.sleep(0.01)
+
